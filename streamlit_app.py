d46f8d8774fd11731b791bc8a3e0d55dfc92b9f7
Arun Arunachalam <doublearhouse@gmail.com>
2023-10-21 14:20:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ def main():
 
     # Submit button
     if st.button("Submit"):
-        messages=[{"role": "user", "content": As an intelligent AI model, if you could be any fictional character, who would you choose and why?}]
+        messages=[{"role": "user", "content": "As an intelligent AI model, if you could be any fictional character, who would you choose and why?"}]
         response = openai.ChatCompletion.create(
             model="gpt-4",
             max_tokens=100,
