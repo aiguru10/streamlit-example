85f195a60e01d8652aa45329f1f69cfd29c64709
Arun Arunachalam <doublearhouse@gmail.com>
2023-10-21 14:19:34 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,14 @@ def main():
 
     # Submit button
     if st.button("Submit"):
+        messages=[{"role": "user", "content": As an intelligent AI model, if you could be any fictional character, who would you choose and why?}]
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            max_tokens=100,
+            temperature=1.2,
+            messages = message)
         # Send the prompt to the OpenAI API
-        response = openai.ChatCompletion.create(engine="gpt-4", prompt=user_input, max_tokens=500)
+        #response = openai.ChatCompletion.create(engine="gpt-4", prompt=user_input, max_tokens=500)
         st.write(response.choices[0].text.strip())
 
 if __name__ == "__main__":
